77bdd92112cbfdb42ac793e0bcbae8abeb89a45a
DanaMichelleZ <dana.zambelli@davinci.edu.ar>
2024-10-10 19:34:24 -0300

    Create test.js

---
+++ semana2/Ejercicio1/test.js
@@ -0,0 +1,7 @@
+const esPrimo = require('./primos.js');  
+
+const numero = 7; 
+console.log(`¿El número ${numero} es primo? ${esPrimo(numero)}`);
+
+const otroNumero = 10;
+console.log(`¿El número ${otroNumero} es primo? ${esPrimo(otroNumero)}`);
