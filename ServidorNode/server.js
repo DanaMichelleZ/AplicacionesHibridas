4f33ae4908dc7f28ac5b83d324b0013dc3ea9406
DanaMichelleZ <dana.zambelli@davinci.edu.ar>
2024-10-10 20:05:07 -0300

    Update server.js

--- ServidorNode/server.js
+++ ServidorNode/server.js
@@ -1,9 +1,13 @@
 const http = require('http');
 
-
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Servidor funcionando</h1>');
+    if (req.url === '/alumno') {
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Alumno: Dana, Comisión: DWN4AV');
+    } else {
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<h1>Servidor funcionando</h1>');
+    }
 });
 
 server.listen(3000, () => {
